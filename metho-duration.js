014c1c234a1bad063da42d546a8fe0f2337b7783
Jon Randy <jonrandy@gmail.com>
2024-07-17 18:45:45 +0700

    WIP

--- metho-duration.js
+++ metho-duration.js
@@ -1,5 +1,7 @@
 import * as Metho from "metho"
 
+export const isCleanImport = (new URL(import.meta.url)).searchParams.get('clean') !== null
+
 // Duration Unit definitions
 const DURATION_UNITS = [
 	{
@@ -47,10 +49,11 @@ const DURATION_UNITS_BY_ID = DURATION_UNITS.reduce((units, unit)=>(units[unit.id
 
 
 // Our composable 'duration' data type
-export const duration = (...parts) => {
+const duration = (...parts) => {
 	const total = parts.reduce((total, current) => total + current, 0)
-	const newDuration = (...durs) => duration(total, ...durs)
-	return Object.assign(newDuration, {
+	const t = []
+	t.push( (...durs) => duration(total, ...durs) )
+	return Object.assign(t[0], {
 		toString: (omitZeroParts = true) => _toString(total, omitZeroParts),
 		valueOf: () => total,
 		in: (...units) => {
@@ -63,11 +66,17 @@ export const duration = (...parts) => {
 		toStringParts(omitZeroParts = true) {
 			return _toStringParts(total, omitZeroParts)
 		},
+		from(startDate) {
+			return new Date(+startDate + total)
+		},
+		before(startDate) {
+			return new Date(+startDate - total)
+		}
 	})
 }
 
 // covenience function to get duration between two dates
-export const timeBetween = (start, end) => {
+const timeBetween = (start, end) => {
 	// timestamps (string or number) or date objects will work
 	start = +start
 	end = +end
@@ -82,12 +91,12 @@ DURATION_UNITS.forEach(({ id, ms }) => {
 		return duration(this * ms)
 	}, { symbolName: id })
 })
-export const { ms, sec, min, hr, day, wk, mon, yr } = syms
+const { ms, sec, min, hr, day, wk, mon, yr } = syms
 
-// Safely extend built-in Date prototype to allow adding durations to dates
-export const addTime = Metho.add(Date.prototype, function addTime(duration) {
-	return new Date(+this + duration)
-})
+// // Safely extend built-in Date prototype to allow adding durations to dates
+// export const addTime = Metho.add(Date.prototype, function addTime(duration) {
+// 	return new Date(+this + duration)
+// })
 
 // convert a duration to an array of unit parts [w, d, h, m, s, ms]
 const _toParts = i => {
@@ -116,3 +125,23 @@ const _toStringParts = (i, omitZeroParts = true) => {
 // convert a duration to a single readable string - optionally omitting zero valued units
 const _toString = (i, omitZeroParts = true) => _toStringParts(i, omitZeroParts).join` `
 
+// a global convenience to get current UNIX timestamp - shorter than Date.now() (don't do it if ?clean)
+if (!isCleanImport) {
+	Object.defineProperty(window, "now", {
+	  get() { return +(new Date()) }
+	})
+}
+
+
+export default {
+	duration,
+	ms,
+	sec,
+	min,
+	hr,
+	day,
+	wk,
+	mon,
+	yr,
+	timeBetween,
+}
